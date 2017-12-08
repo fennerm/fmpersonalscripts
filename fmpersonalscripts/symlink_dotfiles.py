f5431f78bc6c709ca31f502fa4deb244b02373de
fen-mint <fennermacrae@gmail.com>
2017-12-08 15:00:12 -0800

    Script for symlinking dotfiles into the home directory

---
+++ fmpersonalscripts/symlink_dotfiles.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+"""Symlink all dotfiles in the data/dotfiles repo to the home directory"""
+from __future__ import print_function
+from warnings import warn
+
+from plumbum import local
+
+def is_excluded(f, mint_only, arch_only):
+    """Test if a file should be excluded from linking
+
+    Parameters
+    ----------
+    f: local.path
+        File/directory to be linked
+    mint_only: List[str]
+        List of files or directories which should not be linked for Linux Mint
+    arch_only: List[str]
+        List of files or directories which should not be linked for Arch
+
+    Returns
+    -------
+    bool
+        True if should be excluded
+    """
+    os = local.env['OS']
+    if os == 'arch' and (str(f) in mint_only):
+        excluded = True
+    elif os == 'mint' and (str(f) in arch_only):
+        excluded = True
+    else:
+        excluded = False
+    return excluded
+
+def symlink_dir(d, base, mint_only, arch_only):
+    """Symlink a config directory
+
+    If the target exists, emit a warning. Otherwise link all the contained
+    config files, creating directories as necessary
+
+    Parameters
+    ----------
+    d: local.path
+        Directory to be linked
+    base: local.path
+        Directory to link into
+    mint_only: List[str]
+        List of files or directories which should not be linked for Linux Mint
+    arch_only: List[str]
+        List of files or directories which should not be linked for Arch
+    """
+    base = base / d.name
+    if base.exists():
+        warn("DOTFILE WARNING: Conficting file present at " + base)
+    else:
+        base.mkdir()
+    for f in d.list():
+        symlink_dotfile(f, base, mint_only, arch_only)
+
+def symlink_dotfile(f, base, mint_only, arch_only):
+    """Symlink a config file
+
+    If the target exists, emit a warning. Otherwise symlink `f` into `base`
+
+    Parameters
+    ----------
+    f: local.path
+        Directory to be linked
+    base: local.path
+        Directory to link into
+    mint_only: List[str]
+        List of files or directories which should not be linked for Linux Mint
+    arch_only: List[str]
+        List of files or directories which should not be linked for Arch
+    """
+    if f.is_dir():
+        symlink_dir(f, base, mint_only, arch_only)
+    else:
+        if not is_excluded(f, mint_only, arch_only):
+            dest = base / f.name
+            if dest.is_symlink():
+                dest.unlink()
+            if dest.exists():
+                warn("DOTFILE WARNING: Conficting file present at " + dest)
+            else:
+                f.symlink(dest)
+
+def symlink_all_dotfiles(dots, home, mint_only, arch_only):
+    """Symlink all dotfiles into the home directory
+
+    Parameters
+    ----------
+    dots: local.path
+        The directory containing the dotfiles.
+    home: local.path
+        The home directory
+    mint_only: List[str]
+        List of files or directories which should not be linked for Linux Mint
+    arch_only: List[str]
+        List of files or directories which should not be linked for Arch
+    """
+    for f in dots.list():
+        symlink_dotfile(f, home, mint_only, arch_only)
+
+
+if __name__ == "__main__":
+    # Grab arguments from environment variables
+    dots = local.path(local.env["DOTS"])
+    home = local.path(local.env["HOME"])
+    arch_only = local.env["ARCH_ONLY"].split(':')
+    mint_only = local.env["MINT_ONLY"].split(':')
+    symlink_all_dotfiles(home=home, dots=dots, arch_only=arch_only,
+                 mint_only=mint_only)
---
+++ test/test_symlink_dotfiles.py
@@ -0,0 +1,72 @@
+from warnings import filterwarnings
+
+from pytest import fixture
+
+from fmpersonalscripts.symlink_dotfiles import symlink_all_dotfiles
+
+filterwarnings("ignore")
+
+@fixture(name="mock_home")
+def gen_mock_home(sandbox):
+    mock_home = {}
+    mock_home['home'] = sandbox / 'home'
+    mock_home['home'].mkdir()
+    mock_home['xdg_home'] = mock_home['home'] / '.config'
+    mock_home['xdg_home'].mkdir()
+    mock_home['config_file'] = mock_home['xdg_home'] / 'a_file'
+    with mock_home['config_file'].open('w') as f:
+        f.write('foo')
+    mock_home['config_file'].touch()
+    mock_home['broken_link'] = mock_home['home'] / 'broken_link'
+    doesnt_exist = mock_home['home'] / 'doesnt_exist'
+    doesnt_exist.touch()
+    doesnt_exist.symlink(mock_home['broken_link'])
+    doesnt_exist.delete()
+
+    yield mock_home
+    mock_home['home'].delete()
+
+
+@fixture(name="mock_dots")
+def gen_mock_dots(sandbox):
+    mock_dots = {}
+    mock_dots['dots'] = sandbox / 'dots'
+    mock_dots['dots'].mkdir()
+    mock_dots['xdg_home'] = mock_dots['dots'] / '.config'
+    mock_dots['xdg_home'].mkdir()
+    mock_dots['conflict'] = mock_dots['xdg_home'] / 'a_file'
+    mock_dots['conflict'].touch()
+    mock_dots['other_dir'] = mock_dots['dots'] / 'dir2'
+    mock_dots['other_dir'].mkdir()
+    mock_dots['contained_file'] = mock_dots['other_dir'] / 'con_file'
+    mock_dots['contained_file'].touch()
+    mock_dots['no_conflict'] = mock_dots['dots'] / 'another_file'
+    mock_dots['no_conflict'].touch()
+    mock_dots['not_broken_here'] = mock_dots['dots'] / 'broken_link'
+    mock_dots['not_broken_here'].touch()
+    mock_dots['exclude'] = mock_dots['dots'] / 'exclude'
+    mock_dots['exclude'].touch()
+    mock_dots['exclude'] = str(mock_dots['exclude'])
+    yield mock_dots
+    mock_dots['dots'].delete()
+
+@fixture(name="resultant_dirs")
+def gen_resultant_dirs(mock_home, mock_dots):
+    symlink_all_dotfiles(
+        dots=mock_dots['dots'], home=mock_home['home'],
+        arch_only=mock_dots['exclude'], mint_only=mock_dots['exclude'])
+
+def test_conflicts_not_overwritten(resultant_dirs, mock_home, mock_dots):
+    assert mock_home['config_file'].read() == 'foo'
+
+def test_no_conflicts_linked(resultant_dirs, mock_home, mock_dots):
+    assert (mock_home['home'] / 'another_file').exists()
+
+def test_config_dir_not_linked(resultant_dirs, mock_home, mock_dots):
+    assert not mock_home['xdg_home'].is_symlink()
+
+def test_excludes_are_excluded(resultant_dirs, mock_home, mock_dots):
+    assert not (mock_home['home'] / 'exclude').exists()
+
+def test_contained_file_linked(resultant_dirs, mock_home, mock_dots):
+    assert (mock_home['home'] / 'dir2' / 'con_file').exists()
