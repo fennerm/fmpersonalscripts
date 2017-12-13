fce0de4c570773d7c8f63f1e8a106effda3a3bcb
Fenner Macrae <fennermacrae@gmail.com>
2017-12-12 22:03:20 -0800

    Added test case

--- test/test_symlink_dotfiles.py
+++ test/test_symlink_dotfiles.py
@@ -40,13 +40,15 @@ def gen_mock_dots(sandbox):
     mock_dots['other_dir'].mkdir()
     mock_dots['contained_file'] = mock_dots['other_dir'] / 'con_file'
     mock_dots['contained_file'].touch()
-    mock_dots['no_conflict'] = mock_dots['dots'] / 'another_file'
+    mock_dots['no_conflict'] = mock_dots['dots'] / '.another_file'
     mock_dots['no_conflict'].touch()
     mock_dots['not_broken_here'] = mock_dots['dots'] / 'broken_link'
     mock_dots['not_broken_here'].touch()
     mock_dots['exclude'] = mock_dots['dots'] / 'exclude'
     mock_dots['exclude'].touch()
     mock_dots['exclude'] = str(mock_dots['exclude'])
+    mock_dots['two_dots'] = mock_dots['dots'] / '.foo.bar'
+    mock_dots['two_dots'].touch()
     yield mock_dots
     mock_dots['dots'].delete()
 
@@ -60,7 +62,7 @@ def test_conflicts_not_overwritten(resultant_dirs, mock_home, mock_dots):
     assert mock_home['config_file'].read() == 'foo'
 
 def test_no_conflicts_linked(resultant_dirs, mock_home, mock_dots):
-    assert (mock_home['home'] / 'another_file').exists()
+    assert (mock_home['home'] / '.another_file').exists()
 
 def test_config_dir_not_linked(resultant_dirs, mock_home, mock_dots):
     assert not mock_home['xdg_home'].is_symlink()
@@ -70,3 +72,6 @@ def test_excludes_are_excluded(resultant_dirs, mock_home, mock_dots):
 
 def test_contained_file_linked(resultant_dirs, mock_home, mock_dots):
     assert (mock_home['home'] / 'dir2' / 'con_file').exists()
+
+def test_two_dots(resultant_dirs, mock_home, mock_dots):
+    assert (mock_home['home'] / '.foo.bar').exists()
