a72f8b2658b5cc0ccb83a7d79137e3c3b94db546
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-01-29 10:22:40 -0800

    Formatting

--- test/test_init_py_package.py
+++ test/test_init_py_package.py
@@ -7,12 +7,9 @@ def test_init_py_package_produces_expected_output(tmpdir):
     outdir = tmpdir / name
     script = local.path('bin') / 'init_py_package.py'
     with local.cwd(tmpdir):
-        assert_script_produces_files(script=script,
-                                     args=[name],
-                                     output=["setup.py", ".git",
-                                             ".git/hooks/pre-push",
-                                             ".git/hooks/pre-commit",
-                                             name, "test",
-                                             "LICENSE", "README.md",
-                                             '.gitignore'],
-                                     outdir=outdir, empty_ok=True)
+        assert_script_produces_files(
+            script=script, args=[name],
+            output=["setup.py", ".git", ".git/hooks/pre-push",
+                    ".git/hooks/pre-commit", name, "test", "LICENSE",
+                    "README.md", '.gitignore'],
+            outdir=outdir, empty_ok=True)
