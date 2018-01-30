fbc49435a88c010bc6699d0a1643c7a5d97697ed
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-01-30 08:37:34 -0800

    Moved VPN activation out of fsshrc - instead should be activated at startup

--- bin/activate_crc_vpn.sh
+++ bin/activate_crc_vpn.sh
@@ -6,4 +6,5 @@ if ! ifconfig | grep -q tun0; then
     ( sudo openvpn --config "$HOME/data/vpn_certs/schaack-crc.ovpn" ) &
     # Wait for the VPN to fully activate
     sleep 8
+    sync-dotfiles
 fi
