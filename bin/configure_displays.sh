3cd27282dc46c998a27dbc94783039e094c24723
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-02-16 08:38:03 -0800

    Fixed autoconfiguring displays

--- bin/configure_displays.sh
+++ bin/configure_displays.sh
@@ -9,19 +9,18 @@ monitor1="$(xrandr | grep "eDP" | grep " connected" | \
     sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")"
 monitor2="$(xrandr | grep "HDMI" | grep " connected" | \
     sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")"
-monitor3="$(xrandr | grep "HDMI" | grep " connected" | \
+monitor3="$(xrandr | grep "DP-2" | grep " connected" | \
     sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")"
 
 projector_connected="$(xrandr | grep "800x840")"
-dual_monitors_connected="$(xrandr | grep "$monitor3 connected")"
 
-if [ "$dual_monitors_connected" ]; then
+if [ "$monitor3" ]; then
     xrandr --output "$monitor1" --off \
         --output "$monitor2" --auto --output "$monitor3" --auto
 elif [ "$projector_connected" ]; then
     xrandr --output "$monitor2" --mode 800x840
 else
-    xrandr --output "$monitor1" --output "$monitor1" --auto \
+    xrandr --output "$monitor1" --auto \
         --output "$monitor2" --off --output "$monitor3" --off
     xrandr --output "$monitor1" --dpi "$dpi" --scale "$scale"
 fi
