f8789824cda92ed80dccf26244dd71ee272ecb90
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-03-05 21:03:31 -0800

    Don't disable output by default

--- bin/activate_crc_vpn.sh
+++ bin/activate_crc_vpn.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-## Connect to University of IDAHO vpn
+## Connect to IBEST vpn
 
 # Check if already connected
 if ! ifconfig | grep -q tun0; then
-    ( sudo openvpn --config "$HOME/data/vpn_certs/schaack-crc.ovpn" ) &
+    sudo openvpn --config "$HOME/data/vpn_certs/schaack-crc.ovpn" || true
     # Wait for the VPN to fully activate
     sleep 8
     sync-dotfiles
