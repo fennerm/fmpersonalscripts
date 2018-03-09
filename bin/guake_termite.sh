50f2c1913f44a1477cf4269f36f92082d3604c9d
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-03-08 23:11:30 -0800

    Removed old/unecessary scripts

--- bin/guake_termite.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Use tdrop to create a drop down terminal. Command is bound to <F12>.
-# See: https://github.com/noctuid/tdrop
-#      https://github.com/thestinger/termite
-#
-# "-P ‘wmctrl -i -r $wid -b add,above’" Sets the dropdown as always on top.
-tdrop -ma -w 50% -h 100% -y -10 -P 'wmctrl -i -r $wid -b add,above' -s dropdown termite
-export EMACSDOT="/home/data/dotfiles/emacs"
-focus emacs
--- bin/rescue-display.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-## Restore the primary display in case the xrandr autoconfiguration script fails
-
-xrandr --output "$monitor1" --auto 
