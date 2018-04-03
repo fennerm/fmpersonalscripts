fff15ed8ac5c14fd1fafb1f7bfeef2b918f1c6b6
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-04-03 16:18:36 -0700

    Added tests

--- test/test_timer.py
+++ test/test_timer.py
@@ -3,6 +3,17 @@ from pytest import mark
 
 from bin.timer import *
 
+
+@mark.parametrize('input,output', [
+    ('1h30m2s', (1, 30, 2)),
+    ('20s', (0, 0, 20)),
+    ('30m', (0, 30, 0)),
+    ('30M', (0, 30, 0))
+])
+def test_split_time_string(input, output):
+    assert split_time_string(input) == output
+
+
 @mark.parametrize('input,output', [
     ('1h30m2s', 5402),
     ('20s', 20),
