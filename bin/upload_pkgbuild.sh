3419a0749c798d63895bd002937d9506d7473472
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-04-03 16:15:28 -0700

    Removed git repo initialization

--- bin/upload_pkgbuild.sh
+++ bin/upload_pkgbuild.sh
@@ -2,14 +2,9 @@
 ## Upload a PKGBUILD to the aur
 ## Run from the PKGBUILD directory
 
-printf "Initializing git repo...\\n"
-pkgname="${PWD##*/}"
-git remote add "$pkgname" "ssh://aur@aur.archlinux.org/${pkgname}.git"
-git fetch "$pkgname"
-
 printf "Uploading PKGBUILD...\\n"
 makepkg --printsrcinfo > .SRCINFO
-git add PKGBUILD .SRCINFO
+git add -f PKGBUILD .SRCINFO
 git commit -m "Initial commit to AUR"
 git push
 cd ..
