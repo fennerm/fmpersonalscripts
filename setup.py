069536d5a80930f5ef272b6a27336bb86498dc4c
Fenner Macrae <13209544+fennerm@users.noreply.github.com>
2018-06-26 17:04:10 -0700

    Simplified setup script

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 """Setup script"""
+from io import open
 import os
-from setuptools import (
-    find_packages,
-    setup,
-)
+from setuptools import find_packages, setup
 
 # =============================================================================
 # Globals
 # =============================================================================
 """Location of the README file"""
-README = 'README.md'
+README = "README.md"
 
 """Github username"""
-USERNAME = 'fennerm'
+USERNAME = "fennerm"
 
 """Package name"""
-NAME = 'fmpersonalscripts'
+NAME = "fmpersonalscripts"
 
 
 # =============================================================================
@@ -23,39 +21,30 @@ NAME = 'fmpersonalscripts'
 # =============================================================================
 
 
-def long_description(readme=README):
-    """Extract the long description from the README"""
-    try:
-        from pypandoc import convert
-        long_description = convert(str(readme), 'md', 'rst')
-    except (ImportError, IOError, OSError):
-        with open(readme, 'r') as f:
-            long_description = f.read()
-    return long_description
-
-
 def url(name=NAME, username=USERNAME):
     """Generate the package url from the package name"""
-    return '/'.join(['http://github.com', username, name])
+    return "/".join(["http://github.com", username, name])
 
 
 def list_scripts():
     """Get the names of the scripts in the bin directory"""
-    scripts = os.listdir('bin')
+    scripts = os.listdir("bin")
+    scripts = ["bin/" + script for script in scripts]
     scripts = [f for f in scripts if os.path.isfile(f)]
-    scripts = [f for f in scripts if '__init__' not in f]
-    scripts = [f for f in scripts if not f.endswith('.pyc')]
+    scripts = [f for f in scripts if "__init__" not in f]
+    scripts = [f for f in scripts if not f.endswith(".pyc")]
     return scripts
 
 
-setup(name=NAME,
-      version='0.0.1',
-      description=long_description()[0],
-      long_description=long_description(),
-      url=url(),
-      author='Fenner Macrae',
-      author_email='fmacrae.dev@gmail.com',
-      license='MIT',
-      packages=find_packages(exclude=["*test*"]),
-      scripts=list_scripts()
-      )
+setup(
+    name=NAME,
+    version="0.0.1",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url=url(),
+    author="Fenner Macrae",
+    author_email="fmacrae.dev@gmail.com",
+    license="MIT",
+    packages=find_packages(exclude=["*test*"]),
+    scripts=list_scripts(),
+)
